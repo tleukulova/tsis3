b45cad5e8c31cd3ffb9d7b1c428233d46926cbcb
tleukulova <155585792+tleukulova@users.noreply.github.com>
2024-02-05 09:50:18 +0600

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,6 @@
+def convert_temperature():
+    fahrenheit_temperature = int(input())
+    centigrade_temperature = (5 / 9) * (fahrenheit_temperature - 32)
+    print(centigrade_temperature)
+convert_temperature()
+
